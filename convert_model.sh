ad6b2985e66905676927720022b9c72b75234abb
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-18 08:01:18 +0000

    convert model

--- convert_model.sh
+++ convert_model.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-LOAD_DIR='/mnt/pvc-blob-nfs/klyang/tuning_result/olmo2_replicate_0207_format_torch/torch/'
-TARGET_DIR='/mnt/pvc-blob-nfs/klyang/hf_converted_model/olmo2_replicate_0207_format_torch/checkpoint-187000/'
+LOAD_DIR='/mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_finewebedu_0214/'
+TARGET_DIR='/mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_finewebedu_0214_mid/'
 
 PATTERN="tokenizer*.json"
 
 echo "Start converting..."
-python tools/checkpoint/convert.py --loader core --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir /scratch/torch_model --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
+python tools/checkpoint/convert.py --loader core --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir $TARGET_DIR --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
+# python tools/checkpoint/convert.py --loader core --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir /scratch/torch_model --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
 # python tools/checkpoint/convert.py --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir /scratch/torch_model --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
-python weights_conversion/megatron_to_hf.py --input_dir /scratch/torch_model --num_output_shards 1 --output_dir $TARGET_DIR
-cp /mnt/pvc-blob-nfs/xiaoliu2/Sigma1-10b/GK4V16-Q6144-C4096-M10B-lr5e-5-B16M-Phiv2-1016-retry4-90k/$PATTERN $TARGET_DIR
+# python weights_conversion/megatron_to_hf.py --input_dir /scratch/torch_model --num_output_shards 1 --output_dir $TARGET_DIR
+# cp /mnt/pvc-blob-nfs/xiaoliu2/Sigma1-10b/GK4V16-Q6144-C4096-M10B-lr5e-5-B16M-Phiv2-1016-retry4-90k/$PATTERN $TARGET_DIR
 echo "End converting..."
