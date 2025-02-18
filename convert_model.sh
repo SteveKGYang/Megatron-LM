751e0c7a37494485ccafc5736987dabc81f0ae03
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-18 06:41:09 +0000

    convert model

--- convert_model.sh
+++ convert_model.sh
@@ -5,8 +5,8 @@ TARGET_DIR='/mnt/pvc-blob-nfs/klyang/hf_converted_model/ruizhe_test/checkpoint-1
 PATTERN="tokenizer*.json"
 
 echo "Start converting..."
-# python tools/checkpoint/convert.py --loader core --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir /scratch/torch_model --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
-python tools/checkpoint/convert.py --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir /scratch/torch_model --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
+python tools/checkpoint/convert.py --loader core --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir /scratch/torch_model --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
+# python tools/checkpoint/convert.py --model-type GPT --position-embedding-type rope --load-dir $LOAD_DIR --save-dir /scratch/torch_model --target-tensor-parallel-size 1 --target-pipeline-parallel-size 1 --megatron-path ./
 python weights_conversion/megatron_to_hf.py --input_dir /scratch/torch_model --num_output_shards 1 --output_dir $TARGET_DIR
 cp /mnt/pvc-blob-nfs/xiaoliu2/Sigma1-10b/GK4V16-Q6144-C4096-M10B-lr5e-5-B16M-Phiv2-1016-retry4-90k/$PATTERN $TARGET_DIR
 echo "End converting..."
