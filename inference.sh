debaa1b8bf0b8d7f684799f082ebef788f4d2de4
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-18 08:51:34 +0000

    convert model

--- inference.sh
+++ inference.sh
@@ -9,7 +9,7 @@ MODEL_ARGS=(
     --use-checkpoint-args
     --use-mcore-models
     --tensor-model-parallel-size 1
-    --load /mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_finewebedu_0214_mid_core/
+    --load /mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_finewebedu_0214_mid_core_from_legacy/
 )
 
 INFERENCE_SPECIFIC_ARGS=(
