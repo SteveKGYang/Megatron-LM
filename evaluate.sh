a89907b4cd0ddd9a0d2137025aed50291821e2de
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-19 08:39:55 +0000

    Evaluate from megatron

--- evaluate.sh
+++ evaluate.sh
@@ -24,7 +24,7 @@ INFERENCE_SPECIFIC_ARGS=(
     --results-path /mnt/pvc-blob-nfs/klyang
 )
 
-torchrun --nproc-per-node=1 evaluate.py \
+torchrun --nproc-per-node=2 evaluate.py \
     ${TOKENIZER_ARGS[@]} \
     ${MODEL_ARGS[@]} \
     ${INFERENCE_SPECIFIC_ARGS[@]}
