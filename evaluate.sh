ea70612ec5e2ee0a5a7c835585a118dda83f0d6c
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-19 08:36:16 +0000

    Evaluate from megatron

--- evaluate.sh
+++ evaluate.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 TOKENIZER_ARGS=(
@@ -12,19 +12,19 @@ MODEL_ARGS=(
     --use-mcore-models
     --no-load-rng
     --bf16
-    --tensor-model-parallel-size 4
-    --load /mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_dclm_0215
+    --tensor-model-parallel-size 1
+    --load /mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_finewebedu_0214_mid_core
     # --load /home/pretraining/klyang/mount_dir/eu_mount/llama_3B_data_evaluation_finewebedu_0214_mid
 )
 
 INFERENCE_SPECIFIC_ARGS=(
     --attention-dropout 0.0
     --hidden-dropout 0.0
-    --micro-batch-size 4
+    --micro-batch-size 12
     --results-path /mnt/pvc-blob-nfs/klyang
 )
 
-torchrun --nproc-per-node=4 evaluate.py \
+torchrun --nproc-per-node=1 evaluate.py \
     ${TOKENIZER_ARGS[@]} \
     ${MODEL_ARGS[@]} \
     ${INFERENCE_SPECIFIC_ARGS[@]}
