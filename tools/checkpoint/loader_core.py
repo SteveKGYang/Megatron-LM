8b11ac3899803164405e78bbad9887675fc061df
sjtu-yc <yc.ding@sjtu.edu.cn>
2025-02-21 06:00:58 -0800

    1

--- tools/checkpoint/loader_core.py
+++ tools/checkpoint/loader_core.py
@@ -208,7 +208,7 @@ def _load_checkpoint(queue, args):
                 post_process = mpu.is_pipeline_last_stage()
                 model_rank = 0
                 model_ = [model_provider(pre_process, post_process).to(dtype)]
-                print(model_)
+            print(model_)
             margs.consumed_train_samples = 0
             margs.consumed_valid_samples = 0
             margs.exit_on_missing_checkpoint = True
