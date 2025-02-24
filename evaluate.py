3ba0aa1c0891f17533adeb12dc2dd65eb5e381e5
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-24 09:22:17 +0000

    a

--- evaluate.py
+++ evaluate.py
@@ -380,7 +380,7 @@ class EvalHarnessAdaptor(lm_eval.api.model.LM):
             if "do_sample" in kwargs.keys():
                 kwargs.pop("do_sample")
 
-            stop_tokens = [self.tokenizer.encode(i) for i in until]
+            stop_tokens = [self.tokenizer.tokenize(i) for i in until]
             # cont = self.generate(
             #     text=context,
             #     stop_tokens=stop_tokens,
