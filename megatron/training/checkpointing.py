6b211642c17e7eec66dbe22254cd58db34ae42b9
sjtu-yc <yc.ding@sjtu.edu.cn>
2025-02-24 07:56:53 -0800

    1

--- megatron/training/checkpointing.py
+++ megatron/training/checkpointing.py
@@ -917,9 +917,6 @@ def _load_base_checkpoint(
     else:
         checkpoint_name = get_checkpoint_name(load_dir, iteration, release, return_base_dir=False)
 
-    # sync
-    if torch.distributed.is_initialized():
-        torch.distributed.barrier()
     state_dict = torch.load(checkpoint_name, map_location='cpu')
     print("checkpoint name: ", checkpoint_name)
     # print("state_dict: ", state_dict.shape)
