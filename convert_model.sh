c15f079f30426b2234e19456b48a96960dbb3e66
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-27 02:01:13 +0000

    a

--- convert_model.sh
+++ convert_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-LOAD_DIR='/mnt/pvc-blob-nfs/klyang/tuning_result/moe_ckpt_test/'
-TARGET_DIR='/mnt/pvc-blob-nfs/klyang/tuning_result/moe_ckpt_test_tp1_core/'
+LOAD_DIR='/mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_nemotron_0217/'
+TARGET_DIR='/mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_nemotron_tp1_core/'
 
 PATTERN="tokenizer*.json"
 
