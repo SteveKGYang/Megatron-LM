2d6eda00bd262928d1fb687f2822bcd33436866b
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-28 02:57:35 +0000

    a

--- evaluate.sh
+++ evaluate.sh
@@ -22,12 +22,12 @@ MODEL_ARGS=(
 INFERENCE_SPECIFIC_ARGS=(
     --attention-dropout 0.0
     --hidden-dropout 0.0
-    --micro-batch-size 8
-    --results-path /mnt/pvc-blob-nfs/klyang/results_llama3B_dclm_math_continue_mmlu_gsm8k.json
+    --micro-batch-size 12
+    --results-path /mnt/pvc-blob-nfs/klyang/results_llama3B_dclm_math_continue_other.json
     # --results-path /mnt/mydata/klyang/results_olmo_replicate_mmlu_continuation.json
-    # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
-    --task-list gsm8k,mmlu_continuation
-    --num-fewshot 5
+    --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
+    # --task-list gsm8k,mmlu_continuation
+    --num-fewshot 0
     --trust-remote-code
 )
 
@@ -41,7 +41,7 @@ INFERENCE_SPECIFIC_ARGS=(
 #     ${MODEL_ARGS[@]} \
 #     ${INFERENCE_SPECIFIC_ARGS[@]}
 
-accelerate launch --main_process_port 29501 evaluate.py \
+accelerate launch --main_process_port 29500 evaluate.py \
     ${TOKENIZER_ARGS[@]} \
     ${MODEL_ARGS[@]} \
     ${INFERENCE_SPECIFIC_ARGS[@]}
