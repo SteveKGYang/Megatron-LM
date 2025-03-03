02f9ce40ef9ea32e7cb647f8c94bb588cb44de58
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-03-03 02:40:13 +0000

    a

--- evaluate.sh
+++ evaluate.sh
@@ -15,7 +15,7 @@ MODEL_ARGS=(
     --no-load-rng
     --bf16
     --tensor-model-parallel-size 1
-    --load /mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_dclm_0215_tp1_core
+    --load /mnt/pvc-blob-nfs/klyang/tuning_result/llama_3B_data_evaluation_dclm_continue_math_only_0227_tp1_core
     # --load /mnt/mydata/klyang/olmo2_replicate_0207_format_torch_tp1_core
 )
 
@@ -23,7 +23,7 @@ INFERENCE_SPECIFIC_ARGS=(
     --attention-dropout 0.0
     --hidden-dropout 0.0
     --micro-batch-size 12
-    --results-path /mnt/pvc-blob-nfs/klyang/results_llama3B_dclm_mmlu_pro_math.json
+    --results-path /mnt/pvc-blob-nfs/klyang/results_llama3B_dclm_math_continue_mmlu_pro_math.json
     # --results-path /mnt/mydata/klyang/results_olmo_replicate_mmlu_continuation.json
     # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
     # --task-list gsm8k,mmlu_continuation
