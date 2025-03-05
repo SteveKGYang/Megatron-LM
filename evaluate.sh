bed0a8ae078a40641d341b71baf11f840ffc4a6e
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-03-05 12:15:08 +0000

    a

--- evaluate.sh
+++ evaluate.sh
@@ -25,10 +25,10 @@ INFERENCE_SPECIFIC_ARGS=(
     --micro-batch-size 12
     # --results-path /mnt/pvc-blob-nfs/klyang/regmix_results/2.json
     --results-path /mnt/mydata/klyang/llama_3B_data_evaluation_nemotron_HQ_0303_few_shot.json
-    # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
+    --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
     # --task-list gsm8k,mmlu_continuation,mmlu_pro_math
-    --task-list gsm8k,mmlu_continuation,mmlu_pro
-    --num-fewshot 5
+    # --task-list gsm8k,mmlu_continuation,mmlu_pro
+    --num-fewshot 0
     --trust-remote-code
 )
 
