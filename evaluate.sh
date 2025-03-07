c9689d1c3df89fb377910c27f6c678b42da49afb
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-03-07 03:03:40 +0000

    a

--- evaluate.sh
+++ evaluate.sh
@@ -25,11 +25,11 @@ INFERENCE_SPECIFIC_ARGS=(
     --hidden-dropout 0.0
     --micro-batch-size 12
     # --results-path /mnt/pvc-blob-nfs/klyang/regmix_results/2.json
-    --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/evaluation_results/llama_3B_data_evaluation_Phi_0305_other_300K.json
-    --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
+    --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/evaluation_results/llama_3B_data_evaluation_Phi_0305_mmlu_300K.json
+    # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
     # --task-list gsm8k,mmlu_continuation,mmlu_pro_math
-    # --task-list mmlu_continuation
-    --num-fewshot 0
+    --task-list mmlu_continuation
+    --num-fewshot 5
     --trust-remote-code
 )
 
