9398fd068b594d29835366ae284b15e1e38e44de
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-03-11 10:48:53 +0000

    a

--- evaluate_regmix.sh
+++ evaluate_regmix.sh
@@ -41,7 +41,7 @@ TOKENIZER_ARGS=(
 #     ${MODEL_ARGS[@]} \
 #     ${INFERENCE_SPECIFIC_ARGS[@]}
 
-for split_id in $(seq 4 $((64))); do
+for split_id in $(seq 4 $((4))); do
     
     MODEL_ARGS=(
         --use-checkpoint-args
@@ -56,11 +56,11 @@ for split_id in $(seq 4 $((64))); do
         --attention-dropout 0.0
         --hidden-dropout 0.0
         --micro-batch-size 12
-        --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/regmix_results/mmlu_$split_id.json
+        --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/regmix_results_test/mmlu_$split_id.json
         # --results-path /mnt/mydata/klyang/results_olmo_replicate_mmlu_continuation.json
         # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
         # --task-list gsm8k,mmlu_continuation
-        --task-list mmlu_continuation
+        --task-list mmlu_validation
         --num-fewshot 5
         --trust-remote-code
     )
