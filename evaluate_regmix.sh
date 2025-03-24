3c5501d39bdd3cdaa465e82daefd5390bf38993f
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-03-24 03:03:55 +0000

    a

--- evaluate_regmix.sh
+++ evaluate_regmix.sh
@@ -22,13 +22,13 @@ for split_id in $(seq 0 $((267))); do
     INFERENCE_SPECIFIC_ARGS=(
         --attention-dropout 0.0
         --hidden-dropout 0.0
-        --micro-batch-size 6
+        --micro-batch-size 4
         --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/regmix_results_nvidia_dclm_math/mmlu_$split_id.json
         # --results-path /mnt/mydata/klyang/results_olmo_replicate_mmlu_continuation.json
         # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
         --task-list mmlu_continuation
         # --task-list math_continuation
-        --num-fewshot 4
+        --num-fewshot 5
         --trust-remote-code
     )
 
