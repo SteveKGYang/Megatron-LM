733809463b0f553f8328c5a9e71860ac602de88a
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-03-26 08:55:10 +0000

    a

--- evaluate_regmix.sh
+++ evaluate_regmix.sh
@@ -8,7 +8,7 @@ TOKENIZER_ARGS=(
     --tokenizer-type HuggingFaceTokenizer
 )
 
-for split_id in $(seq 201 $((267))); do
+for split_id in $(seq 144 $((267))); do
     
     MODEL_ARGS=(
         --use-checkpoint-args
@@ -22,20 +22,20 @@ for split_id in $(seq 201 $((267))); do
     INFERENCE_SPECIFIC_ARGS=(
         --attention-dropout 0.0
         --hidden-dropout 0.0
-        --micro-batch-size 6
+        --micro-batch-size 16
         --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/regmix_results_nvidia_dclm_math/math_$split_id.json
         # --results-path /mnt/mydata/klyang/results_olmo_replicate_mmlu_continuation.json
         # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
-        # --task-list mmlu_continuation
-        --task-list math_continuation
-        --num-fewshot 4
+        --task-list mmlu_continuation
+        # --task-list math_continuation
+        --num-fewshot 5
         --trust-remote-code
     )
 
     echo ${MODEL_ARGS[@]}
     echo ${INFERENCE_SPECIFIC_ARGS[@]}
 
-    accelerate launch --main_process_port 29502 evaluate_regmix.py \
+    accelerate launch --main_process_port 29503 evaluate_regmix.py \
         ${TOKENIZER_ARGS[@]} \
         ${MODEL_ARGS[@]} \
         ${INFERENCE_SPECIFIC_ARGS[@]}
