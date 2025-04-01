578163157e3d575e94b528749e5ddebd16624308
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-04-01 11:21:40 +0000

    a

--- evaluate_dynamic_data_mixing.sh
+++ evaluate_dynamic_data_mixing.sh
@@ -48,7 +48,7 @@ for model_id in $(seq 0 $((1))); do
         INFERENCE_SPECIFIC_ARGS=(
             --attention-dropout 0.0
             --hidden-dropout 0.0
-            --micro-batch-size 16
+            --micro-batch-size 12
             --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/evaluation_results/llama_160m_data_sampling_dclm_math_tra_eval/$TRAJECTORY_GROUP/$TARGET_TRAJECTORY_DIR/$ckpt_name"_"$model_id/mmlu.json
             --task-list mmlu_continuation
             --num-fewshot 5
