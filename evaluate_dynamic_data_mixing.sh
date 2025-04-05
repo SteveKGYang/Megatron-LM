d9fe4d70819b05c8da2051eafbe65f8d8da6e2bb
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-04-05 07:52:43 +0000

    a

--- evaluate_dynamic_data_mixing.sh
+++ evaluate_dynamic_data_mixing.sh
@@ -1,6 +1,6 @@
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-blobkey="?sv=2023-01-03&st=2025-04-01T02%3A00%3A35Z&se=2025-04-08T02%3A00%3A00Z&skoid=568e5914-ecc1-47fe-b4a8-4007497b49e5&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-04-01T02%3A00%3A35Z&ske=2025-04-08T02%3A00%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=TtUUMOBOczhEFd2GgBNDAmsZ0H13YRVUsqhgET6PDtQ%3D"
+blobkey="?sv=2023-01-03&st=2025-04-05T07%3A36%3A15Z&se=2025-04-12T07%3A36%3A00Z&skoid=568e5914-ecc1-47fe-b4a8-4007497b49e5&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-04-05T07%3A36%3A15Z&ske=2025-04-12T07%3A36%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=Hrif4bKQwNPd2HM8zAla%2BzBUN5z%2Bqxd61ZKWd3m8%2FR4%3D"
 
 TRAJECTORY_GROUP=top_1
 TARGET_TRAJECTORY_DIR=top_1_trajectory_2_dynamic_step_71
@@ -31,7 +31,7 @@ for model_id in $(seq 0 $(($model_count-1))); do
         IFS='_' read -ra ITERATION <<< "${ckpt_name}"
         cur_iter=$((10#${ITERATION[-1]}))
 
-        ./azcopy copy --recursive "https://hptrainingwesteurope.blob.core.windows.net/pretraining/tuning_result/llama_160m_data_sampling_dclm_math/"$TRAJECTORY_GROUP"/"$TARGET_TRAJECTORY_DIR"/"$ckpt_name"/"$blobkey "/scratch/target_model/"
+        ./azcopy copy --recursive "https://hptrainingwesteurope.blob.core.windows.net/pretraining/tuning_result/llama_160m_data_sampling_dclm_math/"$TRAJECTORY_GROUP"/"$TARGET_TRAJECTORY_DIR"/"$ckpt_name"/mp_rank_00/model_optim_rng.pt"$blobkey "/scratch/target_model/$ckpt_name"/mp_rank_00/"
         echo $cur_iter > /scratch/target_model/latest_checkpointed_iteration.txt
 
         ls /scratch/target_model
@@ -75,15 +75,10 @@ for model_id in $(seq 0 $(($model_count-1))); do
 
         echo ${INFERENCE_SPECIFIC_ARGS[@]}
         
-        # accelerate launch --main_process_port 29501 evaluate_regmix.py \
-        #     ${TOKENIZER_ARGS[@]} \
-        #     ${MODEL_ARGS[@]} \
-        #     ${INFERENCE_SPECIFIC_ARGS[@]}
-
-        /home/aiscuser/.local/bin/accelerate launch evaluate.py \
-                ${TOKENIZER_ARGS[@]} \
-                ${MODEL_ARGS[@]} \
-                ${INFERENCE_SPECIFIC_ARGS[@]}
+        accelerate launch --main_process_port 29501 evaluate_regmix.py \
+            ${TOKENIZER_ARGS[@]} \
+            ${MODEL_ARGS[@]} \
+            ${INFERENCE_SPECIFIC_ARGS[@]}
 
         rm -rf /scratch/target_model/*
     )
