15c80dc877b6cc97607057b6677c8029da5116e9
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-05-07 12:58:59 +0000

    a

--- evaluate.sh
+++ evaluate.sh
@@ -26,9 +26,9 @@ INFERENCE_SPECIFIC_ARGS=(
     --hidden-dropout 0.0
     --micro-batch-size 1
     # --results-path /mnt/pvc-blob-nfs/klyang/regmix_results/2.json
-    --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/evaluation_results/data_agent_0504_sft_smooth_tp1_core_openbookqa.json
+    --results-path /mnt/blob-hptrainingwesteurope-pretraining-out/evaluation_results/data_agent_0504_sft_smooth_tp1_core_hellaswag.json
     # --task-list hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa,sciq,logiqa,lambada
-    --task-list openbookqa
+    --task-list hellaswag
     # --task-list gsm8k
     # --task-list minerva_math,gsm8k
     # --task-list mmlu_continuation,math_continuation
