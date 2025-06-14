f89a6314d3645a82a85dc52cf795defa0bb26964
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-06-14 13:45:38 +0000

    a

--- convert_model.sh
+++ convert_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-LOAD_DIR='/mnt/blob-hptrainingwesteurope-pretraining/tuning_result/data_agent_0604_sft_2D_agent_model_corrected/'
-TARGET_DIR='/mnt/blob-hptrainingwesteurope-pretraining-out/tuning_result/data_agent_0604_sft_2D_agent_model_corrected_tp1_core/'
+LOAD_DIR='/mnt/blob-hptrainingwesteurope-pretraining/tuning_result/llama_3B_data_evaluation_nemotron_m_continue_math_only_0607/'
+TARGET_DIR='/mnt/blob-hptrainingwesteurope-pretraining-out/tuning_result/llama_3B_data_evaluation_nemotron_m_continue_math_only_0607_tp1_core/'
 
 PATTERN="tokenizer*.json"
 
